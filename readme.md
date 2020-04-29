92c510ee5d35e03cd7ac74e0460d24f744e0348a
vprime <myself@vincentprime.com>
2020-04-28 20:39:43 -0700

    Update Readme, and differentate this fork from the originator.

--- readme.md
+++ readme.md
@@ -1,6 +1,12 @@
-# interactive-python [![Build Status](https://travis-ci.org/WatchBeam/interactive-python.svg)](https://travis-ci.org/WatchBeam/interactive-python)
+# MixPlay-python
 
-# NOTE: This repo is no longer being maintained by Microsoft.
-You can find more information here: dev.mixer.com/blog/mixplaysdkchanges
+NOTE: This repo is not affiliated with Microsoft
+This is a fork of mixer/interactive-python, a python based Mixplay SDK game client.
+The updates in this repo make possibly breaking changes from the interactive-python package, this is not a drop in replacement for interactive-python.
 
-This is a base implementation of a Beam Interactive 2 game client. Documentation for the protocol and tutorials can be found on our [developer site](https://dev.beam.pro)
+
+This is a base implementation of a MixPlay game client. Documentation for the protocol and tutorials can be found in [mixer developer portal](https://dev.mixer.com/guides/mixplay/introduction)
+
+## @@TODO:
+- create scenes 
+- fix potential [backpressure problems](https://github.com/mixer/interactive-python/issues/9)
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import sys
 from setuptools import setup, find_packages
 
 if sys.version_info < (3, 5):
-    raise Exception("interactive_python makes use of asyncio, async, and"
+    raise Exception("mixplay-python makes use of asyncio, async, and"
                     "await, and therefore requires Python >= 3.5.x")
 
 setup(
-    name='interactive_python',
+    name='mixplay-python',
     version='0.1.0',
-    description='Reference API implementation for Mixer Interactive 2',
+    description='Reference API implementation for MixPlay',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python :: 3.5',
@@ -18,9 +18,9 @@ setup(
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries'
     ],
-    author='Connor Peet',
-    author_email='connor@xbox.com',
-    url='https://github.com/mixer/interactive-python',
+    author='AmazingDrMarz',
+    author_email='drmarz@manonthemoonstudio.com',
+    url='https://github.com/Society-of-Mad-Science/MixPlay-python',
     license='MIT',
     packages=find_packages(exclude=['tests']),
     install_requires=['websockets>=3.3', 'varint>=1.0.2', 'pyee>=3.0.3',
